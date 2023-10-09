c5780a2c78056e5341c5f6c0b57b36418b6b55f2
Francis Earl Soriano Fojas/Server Solutions G /SRPH/Engineer/Samsung Electronics <f.fojas@samsung.com>
2023-10-09 23:43:40 +0800

    Testing translation on JS

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -102,12 +102,11 @@ function initSearch() {
         this.metadataWhitelist = ['position']
 
         for (var i in docs) {
-          console.log(docs[i]);
           {% include lunr/custom-index.js %}
           this.add({
             id: i,
-            title: docs[i].title,
-            content: docs[i].content,
+            title: {%t {{docs[i].title}} %},
+            content: {%t {{docs[i].content}} %},
             {%- if site.search.rel_url != false %}
             relUrl: docs[i].relUrl
             {%- endif %}
