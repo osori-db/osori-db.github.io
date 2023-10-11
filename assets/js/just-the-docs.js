d70d4617cf056f82817a2270b71abf40796470b1
Francis Earl Soriano Fojas/Server Solutions G /SRPH/Engineer/Samsung Electronics <f.fojas@samsung.com>
2023-10-12 00:52:10 +0800

    Fixed switch statement for translated search titles

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -143,6 +143,28 @@ function initSearch() {
         return document.getElementById('title-members').value;
       case 'title.logo':
         return document.getElementById('title-logo').value;
+
+      case 'title.guide':
+        return document.getElementById('title-guide').value;
+      case 'title.userTips':
+        return document.getElementById('title-user-tips').value;
+      case 'title.apiDoc':
+        return document.getElementById('title-api-doc').value;
+      case 'title.contributionGuide':
+        return document.getElementById('title-contribution-guide').value;
+      
+      case 'title.license':
+        return document.getElementById('title-license').value;
+      case 'title.osoriLicenseAndNotice':
+        return document.getElementById('title-osori-license-and-notice').value;
+      case 'title.noticeOnUsageRestrictionByHeavyTraffic':
+        return document.getElementById('title-notice-on-usage-restriction-by-heavy-traffic').value;
+      case 'title.noWarranty':
+        return document.getElementById('title-no-warranty').value;
+      case 'title.docLicense':
+        return document.getElementById('title-doc-license').value;
+      case 'title.codeLicense':
+        return document.getElementById('title-code-license').value;
       
       default:
         return title;
