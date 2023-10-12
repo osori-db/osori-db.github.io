4640779bfeeeb3f9993610ee36ec2901de58d685
Francis Earl Soriano Fojas/Server Solutions G /SRPH/Engineer/Samsung Electronics <f.fojas@samsung.com>
2023-10-12 14:14:39 +0800

    Testing  toggle switch between search index

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -85,6 +85,12 @@ function initSearch() {
   var request = new XMLHttpRequest();
   request.open('GET', '{{ "assets/js/search-data.json" | relative_url }}', true);
 
+  if (window.location.href.includes('/eng/')) {
+    console.log('English')
+  } else {
+    console.log('Korean')
+  }
+
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
       var docs = JSON.parse(request.responseText);
@@ -121,60 +127,64 @@ function initSearch() {
     }
   };
 
-  function getTranslatedTitle(title) {
-    switch (title) {
-      case 'title.home':
-        return document.getElementById('title-home').value;
-      case 'title.faq':
-        return document.getElementById('title-faq').value;
-      case 'title.news':
-        return document.getElementById('title-news').value;
-      case 'title.contact':
-        return document.getElementById('title-contact').value;
-
-      case 'title.about':
-        return document.getElementById('title-about').value;
-      case 'title.motivation':
-        return document.getElementById('title-motivation').value;
-      case 'title.charter':
-        return document.getElementById('title-charter').value;
-      case 'title.members':
-        return document.getElementById('title-members').value;
-      case 'title.logo':
-        return document.getElementById('title-logo').value;
-
-      case 'title.guide':
-        return document.getElementById('title-guide').value;
-      case 'title.userTips':
-        return document.getElementById('title-user-tips').value;
-      case 'title.apiDoc':
-        return document.getElementById('title-api-doc').value;
-      case 'title.contributionGuide':
-        return document.getElementById('title-contribution-guide').value;
-      
-      case 'title.license':
-        return document.getElementById('title-license').value;
-      case 'title.osoriLicenseAndNotice':
-        return document.getElementById('title-osori-license-and-notice').value;
-      case 'title.noticeOnUsageRestrictionByHeavyTraffic':
-        return document.getElementById('title-notice-on-usage-restriction-by-heavy-traffic').value;
-      case 'title.noWarranty':
-        return document.getElementById('title-no-warranty').value;
-      case 'title.docLicense':
-        return document.getElementById('title-doc-license').value;
-      case 'title.codeLicense':
-        return document.getElementById('title-code-license').value;
-      
-      default:
-        return title;
-    }
-  }
-
   request.onerror = function(){
     console.log('There was a connection error');
   };
 
   request.send();
+}
+
+function getTranslatedTitle(title) {
+  switch (title) {
+    case 'title.home':
+      return document.getElementById('title-home').value;
+    case 'title.faq':
+      return document.getElementById('title-faq').value;
+    case 'title.news':
+      return document.getElementById('title-news').value;
+    case 'title.contact':
+      return document.getElementById('title-contact').value;
+
+    case 'title.about':
+      return document.getElementById('title-about').value;
+    case 'title.motivation':
+      return document.getElementById('title-motivation').value;
+    case 'title.charter':
+      return document.getElementById('title-charter').value;
+    case 'title.members':
+      return document.getElementById('title-members').value;
+    case 'title.logo':
+      return document.getElementById('title-logo').value;
+
+    case 'title.guide':
+      return document.getElementById('title-guide').value;
+    case 'title.userTips':
+      return document.getElementById('title-user-tips').value;
+    case 'title.apiDoc':
+      return document.getElementById('title-api-doc').value;
+    case 'title.contributionGuide':
+      return document.getElementById('title-contribution-guide').value;
+    
+    case 'title.license':
+      return document.getElementById('title-license').value;
+    case 'title.osoriLicenseAndNotice':
+      return document.getElementById('title-osori-license-and-notice').value;
+    case 'title.noticeOnUsageRestrictionByHeavyTraffic':
+      return document.getElementById('title-notice-on-usage-restriction-by-heavy-traffic').value;
+    case 'title.noWarranty':
+      return document.getElementById('title-no-warranty').value;
+    case 'title.docLicense':
+      return document.getElementById('title-doc-license').value;
+    case 'title.codeLicense':
+      return document.getElementById('title-code-license').value;
+    
+    default:
+      return title;
+  }
+}
+
+function initSearchEng() {
+  console.log('initSearchEng');
 }
 
 function searchLoaded(index, docs) {
@@ -569,7 +579,12 @@ function activateNav() {
 jtd.onReady(function(){
   initNav();
   {%- if site.search_enabled != false %}
-  initSearch();
+  if (window.location.href.includes('/eng/')) {
+    initSearchEng();
+  } else {
+    console.log('initSearch')
+    initSearch();
+  }
   {%- endif %}
   activateNav();
   scrollNav();
