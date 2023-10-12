066df253900457770dc7585119016f054fd33990
Francis Earl Soriano Fojas/Server Solutions G /SRPH/Engineer/Samsung Electronics <f.fojas@samsung.com>
2023-10-12 14:28:28 +0800

    Testing calling just-the-docs.js methods from search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,1 +1,9 @@
-console.log('Search JS is found');
+{%- if site.search_enabled != false %}
+if (window.location.href.includes('/eng/')) {
+    console.log(getTranslatedTitle('title.about'));
+    console.log('English')
+} else {
+    console.log(getTranslatedTitle('title.about'));
+    console.log('Korean')
+}
+{%- endif %}
