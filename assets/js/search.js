85c94005bf3156bb96d452dfd7f4bbe3d3e8fb54
Francis Earl Soriano Fojas/Server Solutions G /SRPH/Engineer/Samsung Electronics <f.fojas@samsung.com>
2023-10-12 14:36:06 +0800

    getting value of search enabled in javascript

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,4 +1,6 @@
-{%- if site.search_enabled != false %}
+console.log(document.getElementById('is-search-enabled'));
+console.log('{{ site.search_enabled }}');
+
 if (window.location.href.includes('/eng/')) {
     console.log(getTranslatedTitle('title.about'));
     console.log('English')
@@ -6,4 +8,3 @@ if (window.location.href.includes('/eng/')) {
     console.log(getTranslatedTitle('title.about'));
     console.log('Korean')
 }
-{%- endif %}
