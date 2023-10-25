3616cfb1e64302e1527b169898b34de641cdb40b
Francis Earl Soriano Fojas/Server Solutions G /SRPH/Engineer/Samsung Electronics <f.fojas@samsung.com>
2023-10-25 14:01:39 +0800

    Finalized navigation and its mobile responsiveness, and open source count

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -153,19 +153,11 @@ function getTranslatedTitle(title) {
 
     case 'title.about':
       return document.getElementById('title-about').value;
-    case 'title.motivation':
-      return document.getElementById('title-motivation').value;
     case 'title.charter':
       return document.getElementById('title-charter').value;
-    case 'title.members':
-      return document.getElementById('title-members').value;
-    case 'title.logo':
-      return document.getElementById('title-logo').value;
 
     case 'title.guide':
       return document.getElementById('title-guide').value;
-    case 'title.userTips':
-      return document.getElementById('title-user-tips').value;
     case 'title.apiDoc':
       return document.getElementById('title-api-doc').value;
     case 'title.contributionGuide':
@@ -173,16 +165,6 @@ function getTranslatedTitle(title) {
     
     case 'title.license':
       return document.getElementById('title-license').value;
-    case 'title.osoriLicenseAndNotice':
-      return document.getElementById('title-osori-license-and-notice').value;
-    case 'title.noticeOnUsageRestrictionByHeavyTraffic':
-      return document.getElementById('title-notice-on-usage-restriction-by-heavy-traffic').value;
-    case 'title.noWarranty':
-      return document.getElementById('title-no-warranty').value;
-    case 'title.docLicense':
-      return document.getElementById('title-doc-license').value;
-    case 'title.codeLicense':
-      return document.getElementById('title-code-license').value;
     
     default:
       return title;
